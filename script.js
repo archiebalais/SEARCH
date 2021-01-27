8af6be353fadb2b344692df820131b45dbaadc50
archiebalais <archieabalais@gmail.com>
2021-01-27 13:44:31 -0800

    javascript2

--- script.js
+++ script.js
@@ -6,22 +6,68 @@ let button2 = document.getElementById('btn2');
 start();
 
 function start() {
-    text.innerHTML = 'Ava: Hey, I think im in trouble.';
-    button1.innerHTML = 'How so?';
-    button2.innerHTML = 'Why are u texting me this late at night.';
+        text.innerHTML = `You wake up on a stone cold floor. There is a door in front of you.`;
+        button1.innerHTML = 'Open Door';
+        button2.innerHTML = 'Stay still';
 
-    button1.addEventListener('click', replyOne);
-    button2.addEventListener('click', replyTwo);
+        button1.addEventListener('click', openDoor);
+        button2.addEventListener('click', stayStill);
 }
 
-    function replyOne() {
-        text.innerHTML = 'placeholder';
-        button1.style = 'display: none';
-        button2.style = 'display:none';
+    function openDoor() {
+        text.innerHTML = `You walk straight down the hallway but it seems endless.`;
+        button1.innerHTML = 'Walk even more';
+        button2.innerHTML = 'turn left';
+
+        button1.addEventListener('click', walkEvenMore)
+        button2.addEventListener('click', turnLeft) 
     }
 
-    function replyTwo() {
-        text.innerHTML = 'placeholder2';
+    function stayStill() {
+        text.innerHTML = 'DEATH';
         button1.style = 'display: none';
-        button2.style = 'display:none';
+        button2.style = 'display: none';
+    }
+
+    function walkEvenMore() {
+        text.innerHTML = `win placeholder`
+        button1.innerHTML = `choice1`
+        button2.innerHTML = 'choice2'
+
+        button1.addEventListener('click', choice1)
+        button2.addEventListener('click', choice2)
+    }
+
+    function turnLeft() {
+        text.innerHTML = 'DEATH';
+        button1.style = 'display: none';
+        button2.style = 'display: none';
+    }
+
+    function choice1() {
+        text.innerHTML = 'DEATH';
+        button1.style = 'display: none';
+        button2.style = 'display: none';
+    }
+
+    function choice2() {
+        text.innerHTML = `win placeholder222`
+        button1.innerHTML = `choice1`
+        button2.innerHTML = 'choice2'
+
+        button1.addEventListener('click', choice1Again)
+        button2.addEventListener('click', choice2Again)
+    }
+
+    function choice1Again() {
+        text.innerHTML = `win placeholder3`
+        button1.innerHTML = 'choice1'
+        button2.innerHTML = `choice2`
+
+    }
+
+    function choice2Again() {
+        text.innerHTML = 'DEATH';
+        button1.style = 'display: none';
+        button2.style = 'display: none';
     }
