1b431ffeaadf68763d97c86595f91bd5044b0b0e
archiebalais <archieabalais@gmail.com>
2021-01-28 09:29:22 -0800

    player lives

--- script.js
+++ script.js
@@ -3,59 +3,160 @@ let text = document.getElementById('text');
 let button1 = document.getElementById('btn1');
 let button2 = document.getElementById('btn2');
 
+let count = 3;
+let playerLives = document.getElementById('player-lives');
+playerLives.innerHTML = count;
+if 
+
+
 start();
 
     function start() {
-        text.innerHTML = `You wake up on a stone cold floor. There is a door in front of you.`;
-        button1.innerHTML = 'Open Door';
-        button2.innerHTML = 'Stay still';
+        text.innerHTML = `Unknown ID: I have her.`;
+        button1.innerHTML = 'Who are you?';
+        button2.innerHTML = `This isn't funny`;
 
-        button1.addEventListener('click', openDoor);
-        button2.addEventListener('click', stayStill);
+        button1.addEventListener('click', replyOne);
+        button2.addEventListener('click', replyOne);
     }
 
-    function openDoor() {
-        text.innerHTML = `You walk straight down the hallway but it seems endless.`;
-        button1.innerHTML = 'Walk even more';
-        button2.innerHTML = 'turn left';
+    function replyOne() {
+        text.innerHTML = `Unknown ID: I'm someone very well acquainted with your sister.`;
+        button1.innerHTML = `Do you know where she is?`;
+        button2.innerHTML = `Stop trolling. I can't deal with this right now.`;
 
-        button1.addEventListener('click', walkEvenMore);
-        button2.addEventListener('click', turnLeft);
+        button1.addEventListener('click', whereSheIs);
+        button2.addEventListener('click', stopTrolling);
+    }
+    
+// right
+    function whereSheIs() {
+        text.innerHTML = `Unknown ID: I can't answer that right now.`;
+        button1.innerHTML = `Don't make me call the police.`;
+        button2.innerHTML = `I'll do whatever you say. Just give back my sister.`;
+
+        button1.addEventListener('click', callThePolice);
+        button2.addEventListener('click', giveBackSister);
+        console.log('hello')
     }
 
-    function stayStill() {
-        text.innerHTML = "death";
-        button1.innerHTML = 'Restart';
-        button1.addEventListener('click', start);
+// wrong
+    function stopTrolling() {
+        text.innerHTML = `Unknown ID: Don't make me angry.`;
+        button1.innerHTML = `I'm Sorry. I'll do whatever you want.`;
         button2.style = "display: none";
+
+        count --;
+        playerLives.innerHTML = count;
+
+        button1.addEventListener('click', giveBackSister);
     }
 
-    function walkEvenMore() {
-        text.innerHTML = "win.You find yourself...";
-        button1.innerHTML = "A";
-        button2.innerHTML = "B";
-
-        button1.addEventListener('click', choiceA)
-        button2.addEventListener('click', choiceB)
-    }
-
-    function turnLeft() {
-        text.innerHTML = "death";
-        button1.style = "display: none";
+// wrong
+    function callThePolice() {
+        text.innerHTML = `Unknown ID: Don't make me angry.`;
+        button1.innerHTML = `I'm Sorry. I'll do whatever you want.`;
         button2.style = "display: none";
+
+        count --;
+        playerLives.innerHTML = count;
+
+        button1.addEventListener('click', giveBackSister)
     }
 
-    function choiceA() {
-        text.innerHTML = "you made choice A";
-        button1.innerHTML = "test";
-        button2.innerHTML = "test A";
+ 
+    function giveBackSister() {
+        text.innerHTML = `Unknown ID: I'll give you a hint on her location. One enters me blind, and comes out seeing. What am I?`;
+        button1.innerHTML = `A school?`;
+        button2.innerHTML = `Death?`
+        button2.style = 'display: block';
+
+        button1.addEventListener('click', aSchool);
+        button2.addEventListener('click', death);
     }
 
-    function choiceB() {
-        text.innerHTML = "you made choice B";
-        button1.innerHTML = "test";
-        button2.innerHTML = "test B"
-        button1.style = "display: block"
-        button2.style = "display: block"
-        button2.removeEventListener()
+// wrong
+    function death() {
+        text.innerHTML = 'Unknown ID: Incorrect.';
+        button1.innerHTML = 'Continue...';
+        button2.style = 'display: none';
+
+        count --;
+        playerLives.innerHTML = count;
+
+        button1.addEventListener('click', aSchool);
+    }
+
+    function aSchool() {
+        text.innerHTML = 'Unknown ID: Now you know where.';
+        button1.innerHTML = `Youre crazy!`;
+        button2.innerHTML = `I don't care. I just want her back.`;
+        button2.style = 'display: block';
+
+        button1.addEventListener('click', youCrazy);
+        button2.addEventListener('click', wantHerBack);
+    }
+
+// wrong
+    function youCrazy() {
+        text.innerHTML = `Unknown ID: Don't call me crazy. That angers me.`;
+        button1.innerHTML = 'Continue...';
+        button2.style = 'display: none';
+
+        count --;
+        playerLives.innerHTML = count;
+
+        button1.addEventListener('click', wantHerBack);
+    }
+
+    function wantHerBack() {
+        text.innerHTML = `Unknown ID: She's safe and sound somewhere locked in the abandoned school at Burnett St... Now where exactly do you think she is in the campus?`;
+        button1.innerHTML = 'Auditorium?';
+        button2.innerHTML = 'Gym?';
+        button2.style = 'display: block';
+
+        button1.addEventListener('click', auditorium);
+        button2.addEventListener('click', gym);
+    }
+
+// wrong
+    function auditorium() {
+        text.innerHTML = 'Unknown ID: Incorrect.';
+        button1.innerHTML = 'Continue...';
+        button2.style = 'display: none';
+
+        count --;
+        playerLives.innerHTML = count;
+
+        button1.addEventListener('click', gym)
+    }
+
+    function gym() {
+        text.innerHTML = 'Unknown ID: Now you know where she is... Now I have one last riddle for you. But be careful, if you get this wrong I promise you will never see her again.';
+        button1.innerHTML = `Okay... what is it?`;
+        button2.style = 'display: none';
+
+        button1.addEventListener('click', finalRiddle);
+    }
+
+    function finalRiddle() {;
+        text.innerHTML = 'final riddle'
+        button1.innerHTML = 'test';
+        button2.innerHTML = 'test2';
+        button2.style = 'display: block';
+
+        button1.addEventListener('click', winScreen);
+        button2.addEventListener('click', losingScreen);
+    }
+
+    function winScreen() {
+        text.innerHTML = 'WIN';
+        button1.style = 'display: none';
+        button2.style = 'display: none';
+    }
+
+    function losingScreen() {
+        text.innerHTML = 'LOSE';
+        button1.style = 'display: none';
+        button2.style = 'display: none';
     }
